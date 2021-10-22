2ba00230016a2f6b2ef52262507ee958ade6b20b
David Zavalla <davidzavalla@gmail.com>
2021-10-22 16:44:41 -0300

    tabla de partidas

---
+++ src/componentes/ListaPartidas.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+export default function ListaPartidas() {
+  const test = [
+    {
+      id_partida: 0,
+      nombre_partida: "Prueba 1asdajskdajsdakdlasdjskadalsdjaksldajdajsldasdakldljsadajslaklaskld",
+      anfitrion: "Carlitosssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+      cant_jugadores: 3
+    },
+    {
+      id_partida: 1,
+      nombre_partida: "Prueba 2",
+      anfitrion: "Pepito",
+      cant_jugadores: 2
+    },
+    {
+      id_partida: 2,
+      nombre_partida: "Prueba 3",
+      anfitrion: "Juancito",
+      cant_jugadores: 5
+    },
+    {
+      id_partida: 2,
+      nombre_partida: "Prueba 3",
+      anfitrion: "Pedrito",
+      cant_jugadores: 6
+    }
+  ];
+
+  const [partidas, setPartidas] = useState(test);
+
+  return (
+    <div style={{maxWidth: '750px', margin: 'auto'}}>
+      <h1>Unirse a una partida</h1>
+      <table className='tablaPartidas'>
+        <thead>
+          <th width='50%'>Partida</th>
+          <th>Anfitrión</th>
+          <th width='130px'>Cant. Jugadores</th>
+        </thead>
+        <tbody>
+          {partidas.map((e, key) => (
+            <tr key={key}>
+              <td>{e.nombre_partida}</td>
+              <td>{e.anfitrion}</td>
+              <td><i class="bi-people-fill"></i> {e.cant_jugadores}/6</td>
+            </tr>
+          ))}
+          {
+          partidas.length === 0 ? 
+            <tr style={{pointerEvents: 'none'}}>
+              <td colSpan={3}><i style={{color: 'gray'}}>No hay partidas</i></td>
+            </tr>
+          : null}
+        </tbody>
+      </table>
+    </div>
+  )
+}
