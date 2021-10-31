5239afee864a2f8d4f5f221662f967b4504f8ec1
Diserio <gringouvieyra@gmail.com>
2021-10-31 15:55:05 -0300

    Coding Style 2.0

--- src/componentes/BotonUnirse.js
+++ src/componentes/BotonUnirse.js
@@ -1,9 +1,6 @@
 import React from "react-router-dom";
 
 export default function BotonUnirse(props) {
-    /* var exampleSocket = new WebSocket("ws://www.example.com/socketserver");
-    exampleSocket.send("Here's some text that the server is urgently awaiting!"); */
-
     return (
         <div>
             <button
