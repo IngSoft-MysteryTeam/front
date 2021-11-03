db2680311b4320de9b7b68f74d02b919f58e44d2
David Zavalla <davidzavalla@gmail.com>
2021-11-03 20:16:46 -0300

    test de redireccion al lobby

--- src/tests/ListaPartidas.test.js
+++ src/tests/ListaPartidas.test.js
@@ -1,6 +1,8 @@
 import { render, waitFor, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ListaPartidas from "../componentes/ListaPartidas";
-import { traerPartidas, unirJugador } from "../services";
+import App from "../App";
+import { traerPartidas, unirJugador, nuevoJugador } from "../services";
 
 jest.mock('../services/index')
 // escondo el boton crear asi no se queja de que uso <Link> fuera de un <Router>
@@ -38,4 +40,37 @@ test('la tabla muestra los datos correctamente', async () => {
   await waitFor(() => expect(screen.getByText('Test')))
   await waitFor(() => expect(screen.getByText('Santi')))
   await waitFor(() => expect(screen.getByText('3/6')))
+})
+
+test('al clickear ingresar, te redirige al lobby de la partida', async () => {
+  render(<App />);
+  
+  let input = screen.getByRole('textbox')
+  let submit = screen.getByRole('button')
+
+  nuevoJugador.mockResolvedValue(Promise.resolve({status: 200, data: null}))
+  traerPartidas.mockResolvedValue(Promise.resolve({data: [{
+    nombre: "Test",
+    anfitrion: "Santi",
+    cantidad_jugadores: 1
+  }]}))
+
+  userEvent.type(input, 'David')
+  userEvent.click(submit)
+
+  unirJugador.mockResolvedValue(Promise.resolve({status: 200, data: {
+    id_jugador: 2,
+    id_partida: 1,
+    anfitrion: "Santi",
+    jugadores: [
+      {nombre: "Santi"},
+      {nombre: "David"}
+    ]
+  }}))
+
+  const unirse = await screen.findByRole('button', {name: 'Unirse'})
+
+  userEvent.click(unirse)
+
+  expect(await screen.findByText('David'))
 })
