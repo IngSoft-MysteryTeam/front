b0bf4ccbe2d6e3cb7ca0c11a04b1ed0822d19266
David Zavalla <davidzavalla@gmail.com>
2021-11-06 18:54:32 -0300

    entradas a los recintos en el tablero

--- src/component/Casillero.js
+++ src/component/Casillero.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Ficha from './Ficha'
 import { obtNombrejugador, moverFicha } from "../services"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown, faArrowLeft, faArrowRight, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 function esEntrada(x, y) {
   if (x === 0 || x === 19) {
@@ -34,6 +36,102 @@ function miColor(jugadores) {
   return jugadores.find(e => e.nombre === obtNombrejugador()).color
 }
 
+function esEntradaRecinto(x, y) {
+  const entradas = [
+    {
+      x: 4,
+      y: 6,
+      color: "#3F3C29",
+      direccion: faArrowDown
+    },
+    {
+      x: 3,
+      y: 13,
+      color: "#3F3C29",
+      direccion: faArrowUp
+    },
+    {
+      x: 6,
+      y: 10,
+      color: "#3F3C29",
+      direccion: faArrowLeft
+    },
+    {
+      x: 6,
+      y: 2,
+      color: "black",
+      direccion: faArrowLeft
+    },
+    {
+      x: 6,
+      y: 15,
+      color: "black",
+      direccion: faArrowLeft
+    },
+    {
+      x: 10,
+      y: 6,
+      color: "#943C29",
+      direccion: faArrowUp
+    },
+    {
+      x: 13,
+      y: 4,
+      color: "black",
+      direccion: faArrowRight,
+    },
+    {
+      x: 13,
+      y: 10,
+      color: "#3F3C29",
+      direccion: faArrowRight,
+    },
+    {
+      x: 13,
+      y: 16,
+      color: "black",
+      direccion: faArrowRight,
+    },
+    {
+      x: 15,
+      y: 6,
+      color: "#3F3C29",
+      direccion: faArrowDown
+    },
+    {
+      x: 10,
+      y: 13,
+      color: "#943C29",
+      direccion: faArrowDown
+    },
+    {
+      x: 16,
+      y: 13,
+      color: "#3F3C29",
+      direccion: faArrowUp
+    }
+  ]
+
+  let entrada = entradas.find(e => e.x === x && e.y === y)
+
+  if (entrada) {
+    return (
+      <div 
+        className='casillero' 
+        style={{
+          backgroundColor: entrada.color,
+          position: 'absolute',
+          fontSize: '1.5em'
+        }}
+      >
+        <FontAwesomeIcon icon={entrada.direccion} />
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
 export default function Casillero(props) {
   const [hovering, setHovering] = useState(false);
 
@@ -52,7 +150,7 @@ export default function Casillero(props) {
       ))}
       {esPosicionPosible(props.posDisponibles, props.x, props.y) ?
       <div style={{opacity: hovering ? '50%' : '0%', cursor: hovering ? 'pointer': '', position: 'absolute'}}
-        onClick={e => {
+      onClick={e => {
           moverFicha({
             id_partida: props.id_partida,
             id_jugador: props.id_jugador,
@@ -62,6 +160,7 @@ export default function Casillero(props) {
         }}>
         <Ficha color={miColor(props.jugadores)} />
       </div> : null}
+      {esEntradaRecinto(props.x, props.y)}
     </div>
   )
 }
