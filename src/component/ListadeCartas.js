01e1f85c612306c7fb74e8d656fe795d9f2a8574
David Zavalla <davidzavalla@gmail.com>
2021-11-06 18:55:52 -0300

    simplificar seleccion de victima y monstruo

--- src/component/ListadeCartas.js
+++ src/component/ListadeCartas.js
@@ -1,53 +1,95 @@
-import React, {Fragment, useState} from 'react'
-import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
+import React, { useState } from 'react'
 import { hacerSospecha } from '../services';
-import Carta from './Carta';
 /**
  * Lista de cartas para 
  */
 export default function ListadeCartas(props) {
-    const [dropdown1, setDropdown1] = useState(false);
-    const [dropdown2, setDropdown2] = useState(false);
     const [victima, setVictima] = useState("")
     const [monstruo, setMonstruo] = useState("")
-    const abricerrarDropdown1=()=>{
-        setDropdown1(!dropdown1)
-    }
-    const abricerrarDropdown2=()=>{
-        setDropdown2(!dropdown2)
-    }
+
+    const victimas = [
+        {
+            nombre: 'Conde',
+            valor: 'CONDE'
+        },
+        {
+            nombre: 'Condesa',
+            valor: 'CONDESA'
+        },
+        {
+            nombre: 'Ama de llaves',
+            valor: 'AMADELLAVES'
+        },
+        {
+            nombre: 'Mayordomo',
+            valor: 'MAYORDOMO'
+        },
+        {
+            nombre: 'Doncella',
+            valor: 'DONCELLA'
+        },
+        {
+            nombre: 'Jardinero',
+            valor: 'JARDINERO'
+        }
+    ];
+
+    const monstruos = [
+        {
+            nombre: 'Drácula',
+            valor: 'DRACULA'
+        }, 
+        {
+            nombre: 'Frankenstein',
+            valor: 'FRANKENSTEIN'
+        },
+        {
+            nombre: 'Hombre Lobo',
+            valor: 'HOMBRELOBO'
+        },
+        {
+            nombre: 'Fantasma',
+            valor: 'FANTASMA'
+        },
+        {
+            nombre: 'Momia',
+            valor: 'MOMIA'
+        },
+        {
+            nombre: 'Dr. Jekyll & Mr. Hyde',
+            valor: 'JEKYLLHYDE'
+        }
+    ];
 
     return(
-        <Fragment>
-        <Dropdown isOpen={dropdown1} toggle={abricerrarDropdown1}>
-            <DropdownToggle caret>
-                Victimas
-            </DropdownToggle>
-            <DropdownMenu>
-                <DropdownItem onClick={e=>setVictima("CONDE")}> CONDE  </DropdownItem>
-                <DropdownItem onClick={e=>setVictima("CONDESA")}> CONDESA  </DropdownItem>
-                <DropdownItem onClick={e=>setVictima("AMADELLAVES")}> AMA DE LLAVES  </DropdownItem>
-                <DropdownItem onClick={e=>setVictima("MAYORDOMO")}> MAYOR DOMO  </DropdownItem>
-                <DropdownItem onClick={e=>setVictima("DONCELLA")}> DONCELLA  </DropdownItem>
-                <DropdownItem onClick={e=>setVictima("JARDINERO")}> JARDINERO  </DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
-            <Dropdown isOpen={dropdown2} toggle={abricerrarDropdown2}>
-            <DropdownToggle caret>
-                Monstruos
-            </DropdownToggle>
-            <DropdownMenu>
-                <DropdownItem onClick={e=>setMonstruo("DRACULA")}> DRACULA </DropdownItem>
-                <DropdownItem onClick={e=>setMonstruo("FRANKENSTEIN")}> FRANKENSTEIN </DropdownItem>
-                <DropdownItem onClick={e=>setMonstruo("HOMBRELOBO")}> HOMBRE LOBO </DropdownItem>
-                <DropdownItem onClick={e=>setMonstruo("FANTASMA")}> FANTASMA </DropdownItem>
-                <DropdownItem onClick={e=>setMonstruo("MOMIA")}> MOMIA </DropdownItem>
-                <DropdownItem onClick={e=>setMonstruo("JEKYLLHYDE")}> Dr.JEKYL y Mr.LHYDE </DropdownItem>
-            </DropdownMenu>
-            </Dropdown>
-            <div style={{display: "flex"}}>
-                <img src={`/cartas/${victima}.png`} height='200px'/>
-                <img src={`/cartas/${monstruo}.png`} height='200px'/>
+        <>
+            <div style={{display: 'flex'}}>
+                <div style={{width: '50%'}}>
+                    <div style={{paddingLeft: '5px', paddingRight: '5px'}}>
+                        <select className="form-select form-select-sm" onChange={e => setVictima(e.target.value)}>
+                            <option selected>Víctimas</option>
+                            {
+                                victimas.map((e, i) => (
+                                    <option value={e.valor}>{e.nombre}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    {victima !== "" ? <img style={{marginTop: '10px'}} src={`/cartas/${victima}.png`} height='200px' alt={victima} /> : null}
+                </div>
+                <div style={{width: '50%'}}>
+                    <div style={{paddingLeft: '5px', paddingRight: '5px'}}>
+                        <select className="form-select form-select-sm" onChange={e => setMonstruo(e.target.value)}>
+                            <option selected>Monstruos</option>
+                            {
+                                monstruos.map((e, i) => (
+                                    <option value={e.valor}>{e.nombre}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    {monstruo !== "" ? <img style={{marginTop: '10px'}} src={`/cartas/${monstruo}.png`} height='200px' alt={monstruo} /> : null}
+                </div>
             </div>
             <div>
                 {
@@ -66,6 +108,6 @@ export default function ListadeCartas(props) {
                 </button> : null
                 }
             </div>
-        </Fragment>
+        </>
     )
 }
