3e93210dd1e96ffd9b64cc1c68df9a0fc1d6c455
David Zavalla <davidzavalla@gmail.com>
2021-11-07 15:14:23 -0300

    documentar entrarRecinto

--- src/services/index.js
+++ src/services/index.js
@@ -109,6 +109,11 @@ export function hacerSospecha(datos) {
     });
 }
 
+/**
+ * Envia al back la accion de entrar a un recinto
+ * @param {object} datos Id de partida y jugador 
+ * @returns 
+ */
 export function entrarRecinto(datos) {
     return axios({
         url: `${baseUrl}partida/${datos.id_partida}/${datos.id_jugador}/entrar-recinto`,
