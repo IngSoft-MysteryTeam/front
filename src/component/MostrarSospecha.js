c619e9f7b882a8d83becefadaf57ef6283bb326a
Diserio <gringouvieyra@gmail.com>
2021-11-08 18:22:16 -0300

    Documentacion

--- src/component/MostrarSospecha.js
+++ src/component/MostrarSospecha.js
@@ -1,8 +1,23 @@
+/**
+ * Renderiza las cartas de una sospecha
+ * @param  {object} props Nombre de jugador y cartas de sospecha
+ */
 export default function MostrarSospecha(props) {
-    return(
-        <div style={{position: "absolute", backgroundColor:"gray", padding:"20px", zIndex:'3', top: '305px', right:'610px' }}>
+    return (
+        <div
+            style={{
+                position: "absolute",
+                backgroundColor: "gray",
+                padding: "20px",
+                zIndex: "3",
+                top: "305px",
+                right: "610px",
+            }}
+        >
             <h1>{props.sospecha.nombre} Sospechó</h1>
-            {props.sospecha.cartas.map(( e, index )=>(<img src={`/cartas/${e}.png`} height='200px'/>))}
+            {props.sospecha.cartas.map((e, index) => (
+                <img src={`/cartas/${e}.png`} height="200px" />
+            ))}
         </div>
-    )    
+    );
 }
