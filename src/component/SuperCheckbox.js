54e7eb3e712dd8d434cb4c21deebe56b18d62ca0
David Zavalla <davidzavalla@gmail.com>
2021-11-11 21:12:51 -0300

    documentacion supercheckbox

--- src/component/SuperCheckbox.js
+++ src/component/SuperCheckbox.js
@@ -1,8 +1,15 @@
-import { faCheck, faQuestion, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+    faCheck,
+    faQuestion,
+    faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
-export default function SuperCheckbox(props) {
+/**
+ * Componente que renderiza un checkbox de 4 estados
+ */
+export default function SuperCheckbox() {
     const [estado, setEstado] = useState(0);
 
     const aumentarEstado = () => {
@@ -11,24 +18,47 @@ export default function SuperCheckbox(props) {
 
     switch (estado) {
         case 0:
-            return <div className="superCheckbox" onClick={e => aumentarEstado()} />;
+            return (
+                <div
+                    className="superCheckbox"
+                    onClick={(e) => aumentarEstado()}
+                />
+            );
         case 1:
             return (
-                <div className="superCheckbox" onClick={e => aumentarEstado()}>
-                    <FontAwesomeIcon icon={faCheck} style={{color: "#0F9D58"}} />
+                <div
+                    className="superCheckbox"
+                    onClick={(e) => aumentarEstado()}
+                >
+                    <FontAwesomeIcon
+                        icon={faCheck}
+                        style={{ color: "#0F9D58" }}
+                    />
                 </div>
-            )
+            );
         case 2:
             return (
-                <div className="superCheckbox" onClick={e => aumentarEstado()}>
-                    <FontAwesomeIcon icon={faTimes} style={{color: "#DB4437"}} />
+                <div
+                    className="superCheckbox"
+                    onClick={(e) => aumentarEstado()}
+                >
+                    <FontAwesomeIcon
+                        icon={faTimes}
+                        style={{ color: "#DB4437" }}
+                    />
                 </div>
-            )
+            );
         case 3:
             return (
-                <div className="superCheckbox" onClick={e => aumentarEstado()}>
-                    <FontAwesomeIcon icon={faQuestion} style={{color: "#F4B400"}} />
+                <div
+                    className="superCheckbox"
+                    onClick={(e) => aumentarEstado()}
+                >
+                    <FontAwesomeIcon
+                        icon={faQuestion}
+                        style={{ color: "#F4B400" }}
+                    />
                 </div>
-            )
+            );
     }
 }
