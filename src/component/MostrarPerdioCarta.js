6a4e70e9a530183744cf0c4fa6f34d4ea3d393c7
David Zavalla <davidzavalla@gmail.com>
2021-11-13 18:43:43 -0300

    borrar div al pedo

--- src/component/MostrarPerdioCarta.js
+++ src/component/MostrarPerdioCarta.js
@@ -2,16 +2,8 @@ export default function MostrarPerdioCarta(props) {
     return (
         <div className="popup">
             <h1>{props.nombre} no usó la Bruja de Salem</h1>
-            <div
-                style={{
-                    display: "flex",
-                    columnGap: "15px",
-                    marginTop: "10px",
-                }}
-            ></div>
             <button
                 className="btn btn-dark"
-                style={{ marginTop: "15px" }}
                 onClick={() => props.setPerdioBruja(null)}
             >
                 Aceptar
