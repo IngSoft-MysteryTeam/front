8556784289e3bc31694390f50f80bc839844b905
David Zavalla <davidzavalla@gmail.com>
2021-11-21 16:27:42 -0300

    arreglar test botonunirse

--- src/tests/BotonUnirse.test.js
+++ src/tests/BotonUnirse.test.js
@@ -1,20 +1,22 @@
 import { screen, render, fireEvent } from "@testing-library/react";
 import BotonUnirse from "../component/BotonUnirse";
 
-test("al clickear en Unirse, se llama a la funcion Unirsepartida con los argumentos adecuados", () => {
+test("al clickear en Unirse, se llama a la funcion setPrePartida con los argumentos adecuados", () => {
     const partida = {
         nombre: "Test",
         anfitrion: "David",
         cantidad_jugadores: 2,
+        colores: ["red", "blue"]
     };
 
     const unirse = jest.fn();
+    const setColor = jest.fn();
 
-    render(<BotonUnirse partida={partida} unirse={unirse} />);
+    render(<BotonUnirse partida={partida} unirse={unirse} setColor={setColor} />);
 
     const button = screen.getByRole("button");
 
     fireEvent.click(button);
 
-    expect(unirse).toHaveBeenLastCalledWith(partida);
+    expect(unirse).toHaveBeenCalledWith(partida);
 });
